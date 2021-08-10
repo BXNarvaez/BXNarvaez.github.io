12f217258acad2ff6126bff23b9a59f439777485
Daniel Narvaez <narvd997@newschool.edu>
2021-08-10 17:04:43 -0400

    testing

---
+++ toggleInfo.js
@@ -0,0 +1,23 @@
+function test()
+{
+    console.log("this is a test.");
+}
+
+// function CollapseAll()
+// {
+
+//     //Takes all project descriptions and collapses them
+//     document.querySelectorAll(".collapsible-box").forEach(element => {
+//         element.style.maxHeight = "9999px";
+//         //element.style.margin = "-5px auto 0px"
+//     })
+// }
+
+const GetTotalHeight = function (int) {
+    let projectInfo = this.nextElementSibling;
+    console.log(projectInfo);
+}
+
+const Toggle = function () {
+    
+}
