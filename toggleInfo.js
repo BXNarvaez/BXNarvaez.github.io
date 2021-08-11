d6e44b51912547cbad66e2141ade30fc866229d4
Daniel Narvaez <narvd997@newschool.edu>
2021-08-11 14:59:37 -0400

    themes pt 2

--- toggleInfo.js
+++ toggleInfo.js
@@ -1,4 +1,15 @@
+const ToggleMenu = function(element) {
+    
+}
+
 const ToggleProjectInfo = function(element) {
     let projectInfo = element.nextElementSibling;
     projectInfo.classList.toggle("active-box")
 }
+
+
+const themeSwitch = document.querySelector('input');
+
+themeSwitch.addEventListener('change', () => {
+  document.body.classList.toggle('dark-mode');
+});
