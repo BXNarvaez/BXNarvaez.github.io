f69feb0b9e02a45e0b4baa3b691e2ae61fb109ec
Daniel Narvaez <narvd997@newschool.edu>
2021-08-10 21:51:18 -0400

    description toggle added

--- toggleInfo.js
+++ toggleInfo.js
@@ -1,23 +1,4 @@
-function test()
-{
-    console.log("this is a test.");
-}
-
-// function CollapseAll()
-// {
-
-//     //Takes all project descriptions and collapses them
-//     document.querySelectorAll(".collapsible-box").forEach(element => {
-//         element.style.maxHeight = "9999px";
-//         //element.style.margin = "-5px auto 0px"
-//     })
-// }
-
-const GetTotalHeight = function (int) {
-    let projectInfo = this.nextElementSibling;
-    console.log(projectInfo);
-}
-
-const Toggle = function () {
-    
+const ToggleProjectInfo = function(element) {
+    let projectInfo = element.nextElementSibling;
+    projectInfo.classList.toggle("active-box")
 }
