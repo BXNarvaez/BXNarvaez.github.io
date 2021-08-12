33214cd8eb491e56c05aeef8a81e3a7888792717
Daniel Narvaez <narvd997@newschool.edu>
2021-08-12 12:06:26 -0400

    local storage. pt 2

--- functions.js
+++ functions.js
@@ -1,7 +1,27 @@
 const themeSwitch = document.querySelector('#theme-switch');
+var theme = localStorage.getItem('data-theme');
+
+const ChangeThemeToLight = function() {
+  document.documentElement.setAttribute("data-theme", 'light');
+  localStorage.setItem('data-theme', 'light');
+}
+
+const ChangeThemeToDark = function() {
+  document.documentElement.setAttribute("data-theme", 'dark');
+  localStorage.setItem('data-theme', 'dark');
+}
+
+const CheckTheme = function() {
+  const localStorageTheme = localStorage.getItem('data-theme');
+  
+  if (localStorageTheme !== null && localStorageTheme === "dark") {
+    ChangeThemeToDark();
+    themeSwitch.checked = true;
+  }
+}
 
 themeSwitch.addEventListener('change', () => {
-  let theme = localStorage.getItem('data-theme');
+  theme = localStorage.getItem('data-theme');
   if (theme === 'dark'){
     ChangeThemeToLight();
   } else {
@@ -9,21 +29,9 @@ themeSwitch.addEventListener('change', () => {
   }   
 });
 
-const ChangeThemeToLight = function() {
-  document.documentElement.setAttribute("data-theme", 'light');
-  localStorage.setItem('data-theme', 'light');
-
-  document.querySelector("body").classList.toggle('dark')
-}
-
-const ChangeThemeToDark = function() {
-  document.documentElement.setAttribute("data-theme", 'dark');
-  localStorage.setItem('data-theme', 'dark');
-
-  document.querySelector("body").classList.toggle('dark')
-}
-
 const ToggleProjectInfo = function(element) {
-    let projectInfo = element.nextElementSibling;
-    projectInfo.classList.toggle("active-box")
+  let projectInfo = element.nextElementSibling;
+  projectInfo.classList.toggle("active-box")
 }
+
+window.onload = CheckTheme();
