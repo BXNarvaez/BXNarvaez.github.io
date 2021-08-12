fa5fe316ea9e0628b8d9d85910890750f83cb5ba
Daniel Narvaez <narvd997@newschool.edu>
2021-08-12 09:29:00 -0400

    localstorage

--- functions.js
+++ functions.js
@@ -1,8 +1,27 @@
-const themeSwitch = document.querySelector('input');
+const themeSwitch = document.querySelector('#theme-switch');
 
 themeSwitch.addEventListener('change', () => {
-  document.body.classList.toggle('dark-mode');
+  let theme = localStorage.getItem('data-theme');
+  if (theme === 'dark'){
+    ChangeThemeToLight();
+  } else {
+    ChangeThemeToDark();
+  }   
 });
+
+const ChangeThemeToLight = function() {
+  document.documentElement.setAttribute("data-theme", 'light');
+  localStorage.setItem('data-theme', 'light');
+
+  document.querySelector("body").classList.toggle('dark')
+}
+
+const ChangeThemeToDark = function() {
+  document.documentElement.setAttribute("data-theme", 'dark');
+  localStorage.setItem('data-theme', 'dark');
+
+  document.querySelector("body").classList.toggle('dark')
+}
 
 const ToggleProjectInfo = function(element) {
     let projectInfo = element.nextElementSibling;
