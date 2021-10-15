b681869f72e9a4831b3b7e6eefba0001cc44aa37
Daniel Narvaez <narvd997@newschool.edu>
2021-10-15 12:34:51 -0400

    stuff

---
+++ cdStudioJavaScript/scripts/printer.js
@@ -0,0 +1,9 @@
+const interval = 1000
+
+const instRate = 333; //The rate of instantiation of the objects for option 2.
+const lifeSpan = 2000; //The lifespan of the objects.
+
+function draw()
+{
+    
+}
